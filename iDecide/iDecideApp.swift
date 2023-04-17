6ab53b89cc009a8285f57d41b84687e6c34de8f6
katelukacs <106710292+katelukacs@users.noreply.github.com>
2023-04-17 09:33:16 -0700

    Initial Commit

---
+++ iDecide/iDecideApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDecideApp.swift
+//  iDecide
+//
+//  Created by Kate Lukacs on 17/04/23.
+//
+
+import SwiftUI
+
+@main
+struct iDecideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
