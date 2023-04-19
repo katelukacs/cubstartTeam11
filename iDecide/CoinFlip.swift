099904b8607edeb23c23c08865ca122fce6ec1ea
katelukacs <106710292+katelukacs@users.noreply.github.com>
2023-04-18 21:58:02 -0700

    Created Struct for big/small buttons, created most views (very basic), alert for name, basic navigation with buttons

---
+++ iDecide/CoinFlip.swift
@@ -0,0 +1,20 @@
+//
+//  CoinFlip.swift
+//  iDecide
+//
+//  Created by Kate Lukacs on 18/04/23.
+//
+
+import SwiftUI
+
+struct CoinFlip: View {
+    var body: some View {
+        Text("Yo I flip ya coin")
+    }
+}
+
+struct CoinFlip_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinFlip()
+    }
+}
--- iDecide/LogIn.swift
+++ iDecide/LogIn.swift
@@ -6,10 +6,56 @@
 //
 
 import SwiftUI
+struct BigButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .fontWeight(.bold)
+            .font(.system(size: 20))
+            .padding()
+            .frame(width: 310.0, height: 60.0)
+            .background(Color("DarkTeal"))
+            .foregroundColor(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 20,style:.continuous))
+    }
+}
+
+struct SmallButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .fontWeight(.bold)
+            .font(.system(size: 20))
+            .padding()
+            .frame(width: 150.0, height: 60.0)
+            .background(Color("DarkTeal"))
+            .foregroundColor(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 20,style:.continuous))
+    }
+}
 
 struct LogIn: View {
+    @State var isPresenting = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack{
+                Text("iDecide").font(.system(size: 64)).bold().foregroundColor(Color("DarkTeal"))
+                Text("Username holder")
+                    .padding()
+                Text("Password holder")
+                    .padding()
+                
+                Button {
+                    isPresenting = true
+                } label: {
+                    Text("LOG IN")
+                }
+                .navigationDestination(isPresented: $isPresenting) {
+                    PreviousDecisions()
+                }
+                .buttonStyle(BigButton())
+            }
+            
+        }
     }
 }
 
