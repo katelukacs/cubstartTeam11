3774cb2870181b5d75f632fa4984b8a7b9dca63c
katelukacs <106710292+katelukacs@users.noreply.github.com>
2023-04-24 16:17:02 -0700

    Add files via upload
    linked to prev decisions after flip with popup window

--- iDecide/CoinFlip.swift
+++ iDecide/CoinFlip.swift
@@ -16,78 +16,84 @@ struct CoinFlip: View {
     @State var offset_y: CGFloat = 0.0
     @State var decision: Bool = false
     @State var coinBool: Bool = false
+    @State var changeView: Bool = false
     func randomBool() -> Bool {
         return arc4random_uniform(2) == 0
     }
     
     var body: some View {
-        VStack {
-            Text("").font(.system(size: 32)).bold().foregroundColor(Color("DarkTeal"))
-                .alert("", isPresented: $presentAlert, actions: {
-                    Button("heads", action: {
-                        if coinBool { decision = true
-                        } else {
-                            decision = false
+        NavigationStack {
+            VStack {
+                Text("").font(.system(size: 32)).bold().foregroundColor(Color("DarkTeal"))
+                    .alert("", isPresented: $presentAlert, actions: {
+                        Button("heads", action: {
+                            if coinBool { decision = true
+                            } else {
+                                decision = false
+                            }
+                        })
+                        Button("tails", action: {
+                            if coinBool { decision = false
+                            } else {
+                                decision = true
+                            }
+                        })
+                    }, message: {
+                        Text("call it in the air")
+                    })
+                    .alert("", isPresented: $presentAlert2, actions: {
+                        Button("ok", action: {changeView = true
+                        })
+//                        Button("flip again", action: {
+//                        })
+                    }, message: {
+                        if decision {
+                            Text("You should go for it")
+                        }
+                        else {
+                            Text("This decision is a no-go")
                         }
                     })
-                    Button("tails", action: {
-                        if coinBool { decision = false
-                        } else {
-                            decision = true
+                    .navigationDestination(isPresented: $changeView) {
+                        PreviousDecisions()
+                    }
+                Text(face)
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .frame(width: 150, height: 150)
+                    .background(Color.brown)
+                    .clipShape(Circle())
+                    .rotation3DEffect(Angle(degrees: rotation), axis: (x: 1, y: 0, z: 0))
+                    .offset(x: 0, y: offset_y)
+                    .onTapGesture {
+                        face = ""
+                        coinBool = randomBool()
+                        presentAlert = true
+                        withAnimation(.linear(duration: 1)) {
+                            rotation += 1080.0
+                            offset_y = -300.0
                         }
-                    })
-                }, message: {
-                    Text("call it in the air")
-                })
-                .alert("", isPresented: $presentAlert2, actions: {
-                    Button("ok", action: {
                         
-                    })
-                    Button("flip again", action: {
-                    })
-                }, message: {
-                    if decision {
-                        Text("You should go for it")
+                        withAnimation(.linear(duration: 1).delay(1)) {
+                            rotation += 1080.0
+                            offset_y = 0.0
+                        }
+                        Task {
+                            try await Task.sleep(nanoseconds: 1_900_000_000)
+                            
+                            if coinBool {
+                                face = "heads"
+                            } else {
+                                face = "tails"
+                            }
+                            try await Task.sleep(nanoseconds: 900_000_000)
+                            
+                            presentAlert2 = true
+                        }
                     }
-                    else {
-                        Text("This decision is a no-go")
-                    }
-                })
-            Text(face)
-                .font(.largeTitle)
-                .foregroundColor(.white)
-                .frame(width: 150, height: 150)
-                .background(Color.brown)
-                .clipShape(Circle())
-                .rotation3DEffect(Angle(degrees: rotation), axis: (x: 1, y: 0, z: 0))
-                .offset(x: 0, y: offset_y)
-            .onTapGesture {
-                face = ""
-                coinBool = randomBool()
-                presentAlert = true
-                withAnimation(.linear(duration: 1)) {
-                    rotation += 1080.0
-                    offset_y = -300.0
-                }
-                        
-                withAnimation(.linear(duration: 1).delay(1)) {
-                        rotation += 1080.0
-                        offset_y = 0.0
-                }
-                Task {
-                    try await Task.sleep(nanoseconds: 1_900_000_000)
-                    
-                    if coinBool {
-                        face = "heads"
-                    } else {
-                        face = "tails"
-                    }
-                    try await Task.sleep(nanoseconds: 900_000_000)
-                        
-                    presentAlert2 = true
-                }
+                Text("decision = " + String(decision))
             }
-            Text("decision = " + String(decision))
+            
         }
     }
 }
