9d1b8343e6ef390a826a308fe6e644fce6766d73
treyriddle <107245520+treyriddle@users.noreply.github.com>
2023-04-23 23:45:10 -0700

    Added Model for pros and cons

---
+++ iDecide/Model/ProsCons.swift
@@ -0,0 +1,14 @@
+//
+//  DecisionVariable.swift
+//  iDecide
+//
+//  Created by Trey on 4/23/23.
+//
+
+import Foundation
+
+struct ProsCons: Hashable, Codable {
+    var id: Int
+    var name: String
+    var score: Int
+}
